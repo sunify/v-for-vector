8739db71b25e0a5a8e0cfaaaa1e7874e26109372
Alex Lunyov <isuntc@gmail.com>
2019-02-08 16:53:49 +0800

    Basic Vector class

--- src/__tests__/vector.test.ts
+++ src/__tests__/vector.test.ts
@@ -2,14 +2,25 @@ import { Vector } from '../vector';
 
 describe('construction', () => {
   test('cartesian', () => {
-    const v1 = Vector.cartesian(10, 15);
+    const v1 = Vector.cartesian(10, 0);
     expect(v1.x).toBe(10);
-    expect(v1.y).toBe(15);
+    expect(v1.y).toBe(0);
+    expect(v1.angle).toBe(0);
+    expect(v1.magnitude).toBe(10);
   });
 
   test('polar', () => {
     const v1 = Vector.polar(0, 10);
+    expect(v1.angle).toBe(0);
+    expect(v1.magnitude).toBe(10);
     expect(v1.x).toBe(10);
     expect(v1.y).toBe(0);
+  });
+});
+
+describe('polar', () => {
+  test('magnitude', () => {
+    const v1 = Vector.cartesian(0, 100);
+    expect(Vector.magnitude(v1)).toBe(100);
   });
 });
--- src/vector.ts
+++ src/vector.ts
@@ -1,19 +1,114 @@
-export class Vector {
-  constructor(
-    public x: number,
-    public y: number,
-  ) {
+type CartesianParams = {
+  x: number;
+  y: number;
+};
 
-  }
+type PolarParams = {
+  angle: number;
+  magnitude: number;
+};
+
+export class Vector {
+  private _x: number | undefined;
+  private _y: number | undefined;
+  private _angle: number | undefined;
+  private _magnitude: number | undefined;
 
   static cartesian(x: number, y: number) {
-    return new Vector(x, y);
+    return new Vector({ x, y });
   }
 
   static polar(angle: number, magnitude: number) {
-    return new Vector(
-      magnitude * Math.cos(angle),
-      magnitude * Math.sin(angle),
-    );
+    return new Vector({ angle, magnitude });
+  }
+
+  static magnitude(v: Vector) {
+    return Math.hypot(v.y, v.x);
+  }
+
+  static angle(v: Vector) {
+    return Math.atan2(v.y, v.x);
+  }
+
+  static dist(v1: Vector, v2: Vector) {
+    return Math.hypot(v1.x + v2.x, v1.y + v2.y);
+  }
+
+  static x(angle: number, magnitude: number) {
+    return magnitude * Math.cos(angle);
+  }
+
+  static y(angle: number, magnitude: number) {
+    return magnitude * Math.sin(angle);
+  }
+
+  public get x() {
+    if (
+      this._x === undefined &&
+      this._magnitude !== undefined &&
+      this._angle !== undefined
+    ) {
+      this._x = Vector.x(this._angle, this._magnitude);
+    }
+    return this._x as number;
+  }
+
+  public set x(x: number) {
+    this._x = x;
+    this._angle = undefined;
+    this._magnitude = undefined;
+  }
+
+  public get y() {
+    if (
+      this._y === undefined &&
+      this._magnitude !== undefined &&
+      this._angle !== undefined
+    ) {
+      this._y = Vector.y(this._angle, this._magnitude);
+    }
+    return this._y as number;
+  }
+
+  public set y(y: number) {
+    this._y = y;
+    this._angle = undefined;
+    this._magnitude = undefined;
+  }
+
+  public get angle() {
+    if (this._angle === undefined) {
+      this._angle = Vector.angle(this);
+    }
+    return this._angle as number;
+  }
+
+  public set angle(angle: number) {
+    this._angle = angle;
+    this._x = undefined;
+    this._y = undefined;
+  }
+
+  public get magnitude() {
+    if (this._magnitude === undefined) {
+      this._magnitude = Vector.magnitude(this);
+    }
+    return this._magnitude as number;
+  }
+
+  public set magnitude(magnitude: number) {
+    this._magnitude = magnitude;
+    this._x = undefined;
+    this._y = undefined;
+  }
+
+  private constructor(params: CartesianParams | PolarParams) {
+    if ((params as PolarParams).angle !== undefined) {
+      this._angle = (params as PolarParams).angle;
+      this._magnitude = (params as PolarParams).magnitude;
+    } else {
+      this._x = (params as CartesianParams).x;
+      this._y = (params as CartesianParams).y;
+    }
   }
 }
